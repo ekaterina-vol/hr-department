6f5807e710552a4309b8d00dcd20ef21a91bf163
User <volkatai@yandex.ru>
2025-10-09 23:33:19 +0300

    first commit

---
+++ src/main/java/com/github/ekaterina-vol/hr-department/App.java
@@ -0,0 +1,10 @@
+package com.github.ekaterina-vol.hr-department;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
