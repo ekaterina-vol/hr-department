1a0da95e7c28543493038ed7e18cfc740f3cf11b
User <volkatai@yandex.ru>
2025-10-15 23:42:59 +0300

    feat(inmemory/post): add crud and business methods

--- src/main/java/com/github/ekaterina_vol/hr_department/domain/repositories/PostRepository.java
+++ src/main/java/com/github/ekaterina_vol/hr_department/domain/repositories/PostRepository.java
@@ -2,5 +2,10 @@ package com.github.ekaterina_vol.hr_department.domain.repositories;
 
 import com.github.ekaterina_vol.hr_department.domain.entities.Post;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository extends Repository<Post, Long> {
+    List<Post> findByDepartment(String department);
+    Optional<Post> findByTitle(String title);
 }
--- src/main/java/com/github/ekaterina_vol/hr_department/domain/repositories/inmemory/PostRepositoryImpl.java
+++ src/main/java/com/github/ekaterina_vol/hr_department/domain/repositories/inmemory/PostRepositoryImpl.java
@@ -3,32 +3,71 @@ package com.github.ekaterina_vol.hr_department.domain.repositories.inmemory;
 import com.github.ekaterina_vol.hr_department.domain.entities.Post;
 import com.github.ekaterina_vol.hr_department.domain.repositories.PostRepository;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 public class PostRepositoryImpl implements PostRepository {
+    private final Map<Long, Post> postStorage = new HashMap<>();
+    private final AtomicLong currentId = new AtomicLong(1);
+
     @Override
     public Optional<Post> create(Post entity) {
-        return Optional.empty();
+        Long nextId = currentId.getAndIncrement();
+
+        Post postToSave = Post.builder()
+                .postId(nextId)
+                .title(entity.getTitle())
+                .department(entity.getDepartment())
+                .build();
+
+        postStorage.put(postToSave.getPostId(), postToSave);
+        return Optional.of(postToSave);
     }
 
     @Override
-    public Optional<Post> findById(Long aLong) {
-        return Optional.empty();
+    public Optional<Post> findById(Long id) {
+        return Optional.ofNullable(postStorage.get(id));
     }
 
     @Override
     public List<Post> findAll() {
-        return null;
+        return new ArrayList<>(postStorage.values());
     }
 
     @Override
     public Optional<Post> update(Post entity) {
-        return Optional.empty();
+        if (entity == null || entity.getPostId() == null || !postStorage.containsKey(entity.getPostId())) {
+            return Optional.empty();
+        }
+
+        Long id = entity.getPostId();
+        Post curPost = postStorage.get(id);
+        if (entity.getTitle().equals(curPost.getTitle()) &&
+        entity.getDepartment().equals(curPost.getDepartment())) {
+            return Optional.of(curPost);
+        }
+
+        postStorage.put(entity.getPostId(), entity);
+        return Optional.of(entity);
     }
 
     @Override
-    public boolean deleteById(Long aLong) {
-        return false;
+    public boolean deleteById(Long id) {
+        return postStorage.remove(id) != null;
+    }
+
+    @Override
+    public List<Post> findByDepartment(String department) {
+        return postStorage.values().stream()
+                .filter(post -> department.equals(post.getDepartment()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<Post> findByTitle(String title) {
+        return postStorage.values().stream()
+                .filter(post -> title.equals(post.getTitle()))
+                .findFirst();
     }
 }
