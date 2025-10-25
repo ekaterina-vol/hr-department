f023150189d00ef3da4888eb5babfc9903e3b9cd
User <volkatai@yandex.ru>
2025-10-25 22:34:33 +0300

    feat(inmemory/employment): add crud and business methods

--- src/main/java/com/github/ekaterina_vol/hr_department/domain/repositories/EmploymentRepository.java
+++ src/main/java/com/github/ekaterina_vol/hr_department/domain/repositories/EmploymentRepository.java
@@ -2,5 +2,13 @@ package com.github.ekaterina_vol.hr_department.domain.repositories;
 
 import com.github.ekaterina_vol.hr_department.domain.entities.Employment;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface EmploymentRepository extends Repository<Employment, Long> {
+    List<Employment> findByEmployeeId(Long employeeId);
+    List<Employment> findByPostId(Long postId);
+    List<Employment> findByStartDate(LocalDateTime startDate);
+    List<Employment> findByEndDate(LocalDateTime endDate);
 }
--- src/main/java/com/github/ekaterina_vol/hr_department/domain/repositories/inmemory/EmploymentRepositoryImpl.java
+++ src/main/java/com/github/ekaterina_vol/hr_department/domain/repositories/inmemory/EmploymentRepositoryImpl.java
@@ -1,34 +1,102 @@
 package com.github.ekaterina_vol.hr_department.domain.repositories.inmemory;
 
+import com.github.ekaterina_vol.hr_department.domain.entities.Employee;
 import com.github.ekaterina_vol.hr_department.domain.entities.Employment;
 import com.github.ekaterina_vol.hr_department.domain.repositories.EmploymentRepository;
 
-import java.util.List;
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 public class EmploymentRepositoryImpl implements EmploymentRepository {
+    private final Map<Long, Employment> employmentStorage = new HashMap<>();
+    private final AtomicLong currentId = new AtomicLong(1);
     @Override
     public Optional<Employment> create(Employment entity) {
-        return Optional.empty();
+        if (employmentStorage.containsKey(entity.getEmploymentId())) {
+            return Optional.empty();
+        }
+
+        Long nextId = currentId.getAndIncrement();
+
+        Employment employmentToSave = Employment.builder()
+                .employmentId(nextId)
+                .employeeId(entity.getEmployeeId())
+                .postId(entity.getPostId())
+                .startDate(entity.getStartDate())
+                .endDate(entity.getEndDate())
+                .build();
+
+        employmentStorage.put(employmentToSave.getEmployeeId(), employmentToSave);
+        return Optional.of(employmentToSave);
     }
 
     @Override
-    public Optional<Employment> findById(Long aLong) {
-        return Optional.empty();
+    public Optional<Employment> findById(Long id) {
+        return Optional.ofNullable(employmentStorage.get(id));
     }
 
     @Override
     public List<Employment> findAll() {
-        return null;
+        return new ArrayList<>(employmentStorage.values());
     }
 
     @Override
     public Optional<Employment> update(Employment entity) {
-        return Optional.empty();
+        if (!employmentStorage.containsKey(entity.getEmploymentId())) {
+            return Optional.empty();
+        }
+
+        Long id = entity.getEmploymentId();
+        Employment curEmployment = employmentStorage.get(id);
+
+        Employment updatedEmployment = Employment.builder()
+                .employmentId(entity.getEmploymentId())
+                .employeeId(entity.getEmployeeId())
+                .postId(entity.getPostId())
+                .startDate(entity.getStartDate())
+                .endDate(entity.getEndDate())
+                .build();
+
+        employmentStorage.put(updatedEmployment.getEmployeeId(), updatedEmployment);
+        return Optional.of(updatedEmployment);
     }
 
     @Override
-    public boolean deleteById(Long aLong) {
-        return false;
+    public boolean deleteById(Long id) {
+        return employmentStorage.remove(id) != null;
+    }
+
+    @Override
+    public List<Employment> findByEmployeeId(Long employeeId) {
+        return employmentStorage.values().stream()
+                .filter(employment ->
+                        employeeId.equals(employment.getEmployeeId()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Employment> findByPostId(Long postId) {
+        return employmentStorage.values().stream()
+                .filter(employment ->
+                        postId.equals(employment.getPostId()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Employment> findByStartDate(LocalDateTime startDate) {
+        return employmentStorage.values().stream()
+                .filter(employment ->
+                        startDate.equals(employment.getStartDate()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Employment> findByEndDate(LocalDateTime endDate) {
+        return employmentStorage.values().stream()
+                .filter(employment ->
+                        endDate.equals(employment.getEndDate()))
+                .collect(Collectors.toList());
     }
 }
